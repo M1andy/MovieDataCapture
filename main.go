a238bf297e3a90b8a81b57bc01cebe6783f91470
M1andy's debian <M1andy@mail.dlut.edu.cn>
2022-10-20 20:59:13 +0800

    测试检索本地文件

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"MovieDataCapture/utils"
 	"fmt"
-	"log"
 )
 
 func main() {
-	config, err := utils.LoadConfig()
-	if err != nil {
-		log.Fatalf("load config error: %v", err)
+	for _, file := range utils.VideoList {
+		if info, err := file.Info(); err == nil {
+			fmt.Printf("file name: %s\n", info.Name())
+		} else {
+			fmt.Printf("%s", err)
+		}
 	}
-	fmt.Printf("%v\n", config)
-
 }
